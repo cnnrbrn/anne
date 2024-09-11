3314d6e6ea81e0ecb5ed08235e479112a88023bb
annechh <142426482+annechh@users.noreply.github.com>
2024-09-11 14:17:48 +0200

    Retrieve API key from response, copy and pasted API key into project for authentication

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = "replace-with-your-key";
+export const API_KEY = "7166711d-5e4e-46d3-8962-1865ce75a3e2";
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
