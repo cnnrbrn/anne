ed9d9ac1acf2bf78c16f24b49240e56c939bc6ea
annechh <142426482+annechh@users.noreply.github.com>
2024-09-11 13:51:37 +0200

    Add user login functionality: fetch API call, handle response, and store access token

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,28 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from "../constants";
+// import { getKey } from "./key";
+
+export async function login({ email, password }) {
+    const body = JSON.stringify({
+        email: email,
+        password: password 
+    });
+    
+    try {
+        const response = await fetch(API_AUTH_LOGIN, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body,
+        });
+
+        if (response.ok) {
+            const {data: {accessToken}} = await response.json();
+            localStorage.setItem('accessToken: ', accessToken);
+            // window.location.href = '/';
+        }
+    } catch (error) {
+        alert('Could not log in to user account')
+        console.log(error);
+    }    
+}
