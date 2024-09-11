634e514097d106854e81f73801686a9365554918
annechh <142426482+annechh@users.noreply.github.com>
2024-09-11 13:47:44 +0200

    add user registration functionality with fetch and error handling

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,29 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+import { API_AUTH_REGISTER } from "../constants";
+
+export async function register({ name, email, password}) {
+  const body = JSON.stringify({
+    name, 
+    email, 
+    password
+  });
+
+  try {
+    const response = await fetch(API_AUTH_REGISTER, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      window.location.href = '/auth/login/'
+      return data;
+    } 
+  } catch (error) {
+    alert('Could not register new user account')
+    console.log(error);
+  }
+}
+
