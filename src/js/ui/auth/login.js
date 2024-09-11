e726567fb25df63c5081f18695540b239ba0da89
annechh <142426482+annechh@users.noreply.github.com>
2024-09-11 13:52:18 +0200

    Added form submission handler for user login

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,10 @@
-export async function onLogin(event) {}
+import { login } from "../../api/auth/login";
+
+export async function onLogin(event) {
+    event.preventDefault();
+    
+    const form = event.target;
+    const loginUserData = Object.fromEntries(new FormData(form));
+    
+    login(loginUserData);
+}
