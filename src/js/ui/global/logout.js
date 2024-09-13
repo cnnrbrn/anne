54796c542f8b6dc63bc4da00d32378d1aeee2377
annechh <142426482+annechh@users.noreply.github.com>
2024-09-13 17:12:41 +0200

    Added eventListener for logout button

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,11 @@
-export function setLogoutListener() {}
+import { onLogout } from "../auth/logout.js"
+
+export function setLogoutListener() {
+    const logoutButton = document.getElementById('logoutButton');
+        logoutButton.addEventListener('click', () => {
+            onLogout();
+        });
+    
+}
+
+console.log('hello');
