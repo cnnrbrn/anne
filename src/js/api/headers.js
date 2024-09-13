f769bfa80695f2838dbbb73908482d671a56dfd0
annechh <142426482+annechh@users.noreply.github.com>
2024-09-13 17:07:05 +0200

    Added Content-Type: 'application/json'

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,7 +1,9 @@
 import { API_KEY } from "./constants";
 
 export function headers() {
-  const headers = new Headers();
+  const headers = new Headers({
+    'Content-Type': 'application/json'
+  });
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
