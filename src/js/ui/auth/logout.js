89948526aa442f18a830c8630fbc501ed76aad07
annechh <142426482+annechh@users.noreply.github.com>
2024-09-13 17:11:57 +0200

    Added localStorage removeItem, alert and redirect

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,1 +1,6 @@
-export function onLogout() {}
+export function onLogout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('userData');
+    alert('Successfully logged out');
+    window.location.href = '/auth/login/';
+}
