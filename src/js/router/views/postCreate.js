6f16b53e0be043c8f7a8c3747c5bf5e6a0094851
annechh <142426482+annechh@users.noreply.github.com>
2024-09-13 17:14:49 +0200

    imported and calling the logout

--- src/js/router/views/postCreate.js
+++ src/js/router/views/postCreate.js
@@ -1,6 +1,8 @@
 import { onCreatePost } from "../../ui/post/create";
 import { authGuard } from "../../utilities/authGuard";
+import { setLogoutListener } from "../../ui/global/logout";
 
+setLogoutListener();
 authGuard();
 
 const form = document.forms.createPost;
