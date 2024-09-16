d2d18963ae119189c3081573f25807fceb955db5
annechh <142426482+annechh@users.noreply.github.com>
2024-09-16 22:44:46 +0200

    onCreatePost event handler to handle form submission for creating posts

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,10 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+
+export async function onCreatePost(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const createNewPost = Object.fromEntries(new FormData(form));
+    // console.log("Form Data:", createNewPost); 
+    createPost(createNewPost)
+}
