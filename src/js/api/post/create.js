9491c45bfd4461461068e537ea46176340358cc3
annechh <142426482+annechh@users.noreply.github.com>
2024-09-16 22:44:09 +0200

    createPost function to handle blog post creation with title, body, tags, and media

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,36 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants"
+import { headers } from "../headers";
+
+
+export async function createPost({ blogTitle, blogText, tags, image, alt  }) {
+    const body = {
+        title: blogTitle,
+        body: blogText,
+        tags: [tags],
+        media: {
+            url: image,
+            alt: alt,
+        }
+    };
+    
+    // console.log("Request Body:", body);
+
+    try {
+        const response = await fetch(API_SOCIAL_POSTS, {
+            method: 'POST',
+            headers: headers(),
+            body: JSON.stringify(body),
+        });
+
+        // console.log("Request Payload:", JSON.stringify(body, null, 2));
+        // console.log(response);
+
+        if (response.ok) {
+            const data = await response.json();
+            // console.log("Post created successfully:", data);
+            return data
+        } 
+    } catch (error) {
+        alert('Failed to create new post')
+    }
+}
