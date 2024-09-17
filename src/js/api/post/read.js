fcad2f4c4e0df9bab82f939fd6f224fa49bb3f6b
annechh <142426482+annechh@users.noreply.github.com>
2024-09-17 17:50:54 +0200

    Added buildQueryParams utility to generate query strings with limit, page, and optional tag

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,4 +1,24 @@
-export async function readPost(id) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+export async function readPost(id) {
+
+    
+}
+
+
+function buildQueryParams(limit, page, tag) {
+    const queryParams = new URLSearchParams({
+        limit: limit.toString(),
+        page: page.toString(),
+    });
+    if (tag) {
+        queryParams.append('tag', tag)
+    }
+    return queryParams.toString();
+}
+
+
 
 export async function readPosts(limit = 12, page = 1, tag) {}
 
