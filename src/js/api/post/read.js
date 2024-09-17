2963f590041fdbbceb8878a0ee6a7f70868d9ec9
annechh <142426482+annechh@users.noreply.github.com>
2024-09-17 17:51:46 +0200

    readPosts function to fetch social posts with pagination and optional tag filtering

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -20,6 +20,25 @@ function buildQueryParams(limit, page, tag) {
 
 
 
-export async function readPosts(limit = 12, page = 1, tag) {}
+export async function readPosts(limit = 12, page = 1, tag) {
+    
+    try {
+        const queryString = buildQueryParams(limit, page, tag)
+        
+        const response = await fetch(`${API_SOCIAL_POSTS}?${queryString}`, {
+            method: 'GET',
+            headers: headers(),
+        })
+        
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data);
+            
+        }
+    
+    } catch (error) {
+        alert('Error loading social posts')
+    }
+}
 
 export async function readPostsByUser(username, limit = 12, page = 1, tag) {}
