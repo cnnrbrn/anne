7804f94ba75ebdb384cfee566f48e564953e1519
annechh <142426482+annechh@users.noreply.github.com>
2024-09-19 00:32:23 +0200

    initialize function to fetch and render social posts on load

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,7 +1,28 @@
 import { setLogoutListener } from "../../ui/global/logout";
 import { readPosts } from "../../api/post/read";
+import { renderSocialPosts } from "../../ui/dom/socialPosts";
 import { authGuard } from "../../utilities/authGuard";
 
+
 setLogoutListener();
-readPosts();
 authGuard();
+
+
+// async function initialize() {
+//     const {posts, meta } = await readPosts();
+//     if (posts && posts.length > 0)
+//         renderSocialPosts(posts, meta)
+    
+    
+//     // console.log('posts: ', posts);
+// }
+
+
+
+async function initialize() {
+    const {posts} = await readPosts();
+    renderSocialPosts(posts)
+    // console.log(posts);
+}
+
+initialize();
