6d29910632faf970105aaa5ee045a75ea783eb55
annechh <142426482+annechh@users.noreply.github.com>
2024-09-19 00:18:58 +0200

    pagination and event listeners for back and next buttons

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,10 +1,80 @@
 import { API_SOCIAL_POSTS } from "../constants";
 import { headers } from "../headers";
 
-export async function readPost(id) {
+let currentPage = 1;
+const limit = 12;
 
+
+
+
+const nextButton = document.getElementById('paginationNext');
+    nextButton.addEventListener('click', async () => {
+        console.log('clicked');
+        
+        currentPage++;
+        loadPosts()
+    });
     
+const backButton = document.getElementById('paginationBack');
+    backButton.addEventListener('click', async () => {
+        console.log('clicked');
+        
+        currentPage--;
+        loadPosts()
+    });
+
+const firstPageButton = document.getElementById('paginationFirstPage');
+    firstPageButton.addEventListener('click', async () => {
+        console.log('click');
+        
+    })
+
+const lastPageButton = document.getElementById('paginationLastPage');
+    lastPageButton.addEventListener('click', async () => {
+
+    })
+
+
+
+
+async function loadPosts() {
+    try {
+        const { posts, meta } = await readPosts(limit, currentPage);
+        if (posts && posts.length > 0) {
+            renderSocialPosts(posts);
+            showPageInfo(meta.currentPage, meta.pageCount, meta.totalCount);
+
+            // const nextButton = document.getElementById('paginationNext');
+            // const backButton = document.getElementById('paginationBack');
+            
+            nextButton.style.display = meta.isLastPage ? 'none' : 'block';
+            backButton.style.display = meta.isFirstPage ? 'none' : 'block';
+            
+            firstPageButton.style.display = meta.isFirstPage ? 'none' : 'block';
+            lastPageButton.style.display = meta.isLastPage ? 'none' : 'block';
+        } 
+    } catch (error) {
+        console.error('Error loading posts: ', error);
+    }
 }
+
+loadPosts()
+
+
+function showPageInfo(currentPage, totalPages, totalCount) {
+    const pageInfoElement = document.getElementById('pageInfo');
+    if (pageInfoElement) {
+        pageInfoElement.textContent = `Page ${currentPage} of ${totalPages} - Total Posts: ${totalCount}`;
+    } else {
+        console.error('No page info element found.');
+    }
+}
+
+
+
+// export async function readPost(id) {
+
+// }
 
 
 function buildQueryParams(limit, page, tag) {
