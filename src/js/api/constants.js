06dc6a6ae820b5a2b560f5c38aea196d874686ae
annechh <142426482+annechh@users.noreply.github.com>
2024-09-19 00:25:54 +0200

    get item from local storage

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -2,6 +2,11 @@
 // In Workflow we will learn how to secure this information
 export const API_KEY = "7166711d-5e4e-46d3-8962-1865ce75a3e2";
 
+export const accessToken = localStorage.getItem('accessToken');
+
+export const userData = JSON.parse(localStorage.getItem('userData'));
+    // const userName = userData?.name;
+
 export const API_BASE = "https://v2.api.noroff.dev";
 
 export const API_AUTH = `${API_BASE}/auth`;
@@ -17,3 +22,5 @@ export const API_SOCIAL = `${API_BASE}/social`;
 export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
 
 export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
+
+
