5e4895e250ccfc3afdbeadf7702111819abb3a66
annechh <142426482+annechh@users.noreply.github.com>
2024-09-19 14:44:50 +0200

    import buildSinglePost

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,18 @@
-alert("Single Post Page");
+// alert("Single Post Page");
+
+
+// import { readSinglePost } from "../../api/post/read";
+
+import { buildSinglePost } from "../../ui/dom/singlePost";
+
+
+// async function loadPost() {
+//     const post = await readSinglePost();
+//     buildSinglePost(post)
+//     console.log(post);
+    
+// }
+
+// loadPost();
+
+buildSinglePost();
