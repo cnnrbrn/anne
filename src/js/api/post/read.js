aa6c5e1ba2135debde0d0d4feead02417ae08535
annechh <142426482+annechh@users.noreply.github.com>
2024-09-20 18:54:09 +0200

    commented out pagination handling due to errors

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,100 +1,103 @@
 import { API_SOCIAL_POSTS } from "../constants";
 import { renderSocialPosts } from "../../ui/dom/socialPosts";
+// import { buildSinglePost } from "../../ui/dom/singlePost";
 import { headers } from "../headers";
 
 let currentPage = 1;
 const limit = 12;
 
+// const nextButton = document.getElementById('paginationNext');
+// const backButton = document.getElementById('paginationBack');
+// const firstPageButton = document.getElementById('paginationFirstPage');
+// const lastPageButton = document.getElementById('paginationLastPage');
+
+    
+// nextButton.addEventListener('click', async () => {
+//     currentPage++;
+//     loadPosts();
+// });
+
+// backButton.addEventListener('click', async () => {
+//     currentPage--;
+//     loadPosts();
+// });
+
+// firstPageButton.addEventListener('click', async () => {
+//     currentPage = 1;
+//     loadPosts();
+// })
+
+// lastPageButton.addEventListener('click', async () => {
+//     const { meta } = await readPosts(limit, currentPage);
+//     currentPage = meta.pageCount;
+//     loadPosts();
+// })
+
+
+// async function loadPosts() {
+//     try {
+//         const { posts, meta } = await readPosts(limit, currentPage);
+
+//         if (posts && posts.length > 0) {
+//             renderSocialPosts(posts);
+//             // showPageInfo(meta.currentPage, meta.pageCount, meta.totalCount);
+            
+//             // nextButton.style.display = meta.isLastPage ? 'none' : 'block';
+//             // backButton.style.display = meta.isFirstPage ? 'none' : 'block';
+//             // firstPageButton.style.display = meta.isFirstPage ? 'none' : 'block';
+//             // lastPageButton.style.display = meta.isLastPage ? 'none' : 'block';
+//         } 
+//     } catch (error) {
+//         console.error('Error loading posts: ', error);
+//     }
+// }
+
+// loadPosts()
+
+
+// function showPageInfo(currentPage, totalPages, totalCount) {
+//     const pageInfoElement = document.getElementById('pageInfo');
+//     if (pageInfoElement) {
+//         pageInfoElement.textContent = `Page ${currentPage} of ${totalPages} - Total Posts: ${totalCount}`;
+//     } else {
+//         console.error('No page info element found.');
+//     }
+// }
 
 
 
-const nextButton = document.getElementById('paginationNext');
-const backButton = document.getElementById('paginationBack');
-const firstPageButton = document.getElementById('paginationFirstPage');
-const lastPageButton = document.getElementById('paginationLastPage');
 
 
-
-nextButton.addEventListener('click', async () => {
-    currentPage++;
-    loadPosts();
-});
-
-backButton.addEventListener('click', async () => {
-    currentPage--;
-    loadPosts();
-});
-
-firstPageButton.addEventListener('click', async () => {
-    currentPage = 1;
-    loadPosts();
-})
-
-    lastPageButton.addEventListener('click', async () => {
-    const { meta } = await readPosts(limit, currentPage);
-    currentPage = meta.pageCount;
-    loadPosts();
-    })
-
-
-
-export async function readSinglePost(id) {
-    const apiUrl = `${API_SOCIAL_POSTS}/${id}`
-
+export async function readSinglePost() {
+    const postId = new URLSearchParams(window.location.search).get('id');
     try {
-        const response = await fetch(apiUrl, {
+        const response = await fetch(`${API_SOCIAL_POSTS}/${postId}`, {
             method: 'GET',
             headers: headers()
         })
+        if (postId) {
+            console.log('Post ID: ', `${API_SOCIAL_POSTS}/${postId}`);
+            
+        };
+        
+        console.log('response ',  response);
 
         if (response.ok) {
             const data = await response.json();
             const post = data.data;
-            console.log('read single post data: ', post);
+            console.log('read single post data: ', data);
             
             return post;
-        }
+        };
     } catch (error) {
-        
-    }
-}
-
-
-async function loadPosts() {
-    try {
-        const { posts, meta } = await readPosts(limit, currentPage);
-
-        if (posts && posts.length > 0) {
-            renderSocialPosts(posts);
-            showPageInfo(meta.currentPage, meta.pageCount, meta.totalCount);
-            
-            nextButton.style.display = meta.isLastPage ? 'none' : 'block';
-            backButton.style.display = meta.isFirstPage ? 'none' : 'block';
-            firstPageButton.style.display = meta.isFirstPage ? 'none' : 'block';
-            lastPageButton.style.display = meta.isLastPage ? 'none' : 'block';
-        } 
-    } catch (error) {
-        console.error('Error loading posts: ', error);
-    }
-}
-
-loadPosts()
-
-
-function showPageInfo(currentPage, totalPages, totalCount) {
-    const pageInfoElement = document.getElementById('pageInfo');
-    if (pageInfoElement) {
-        pageInfoElement.textContent = `Page ${currentPage} of ${totalPages} - Total Posts: ${totalCount}`;
-    } else {
-        console.error('No page info element found.');
-    }
-}
+        alert('failed to load single post');
+    };
+};
 
 
 
-// export async function readPost(id) {
 
-// }
+
 
 
 function buildQueryParams(limit, page, tag) {
@@ -113,9 +116,9 @@ function buildQueryParams(limit, page, tag) {
 export async function readPosts(limit = 12, page = 1, tag) {
     
     try {
-        const queryString = buildQueryParams(limit, page, tag)
+        const queryParam = buildQueryParams(limit, page, tag)
         
-        const response = await fetch(`${API_SOCIAL_POSTS}?${queryString}`, {
+        const response = await fetch(`${API_SOCIAL_POSTS}?${queryParam}`, {
             method: 'GET',
             headers: headers(),
         })
@@ -124,11 +127,11 @@ export async function readPosts(limit = 12, page = 1, tag) {
             const data = await response.json();
             const posts = data.data;
             const meta = data.meta;
-
+            
             console.log('POSTS: ', posts);
             console.log('META: ',meta);
             
-
+            
             return {posts, meta};
         }
     
@@ -136,5 +139,8 @@ export async function readPosts(limit = 12, page = 1, tag) {
         alert('Error loading social posts')
     }
 }
+
+
+
 
 export async function readPostsByUser(username, limit = 12, page = 1, tag) {}
