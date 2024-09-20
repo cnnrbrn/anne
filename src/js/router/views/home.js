ad0073c57c3aa819542c86a44e3653b8f71a8dbe
annechh <142426482+annechh@users.noreply.github.com>
2024-09-20 21:44:51 +0200

    Imported pagination handling from read.js

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -3,15 +3,76 @@ import { readPosts } from "../../api/post/read";
 import { renderSocialPosts } from "../../ui/dom/socialPosts";
 import { authGuard } from "../../utilities/authGuard";
 
+let currentPage = 1;
+const limit = 12;
 
-setLogoutListener();
-authGuard();
+const nextButton = document.getElementById('paginationNext');
+const backButton = document.getElementById('paginationBack');
+const firstPageButton = document.getElementById('paginationFirstPage');
+const lastPageButton = document.getElementById('paginationLastPage');
+
+
+nextButton.addEventListener('click', async () => {
+    currentPage++;
+    loadPosts();
+});
+
+backButton.addEventListener('click', async () => {
+    currentPage--;
+    loadPosts();
+});
+
+firstPageButton.addEventListener('click', async () => {
+    currentPage = 1;
+    loadPosts();
+})
+
+lastPageButton.addEventListener('click', async () => {
+    const { meta } = await readPosts(limit, currentPage);
+    currentPage = meta.pageCount;
+    loadPosts();
+})
 
 
 async function loadPosts() {
+    try {
+        const { posts, meta } = await readPosts(limit, currentPage);
+
+        if (posts && posts.length > 0) {
+            renderSocialPosts(posts);
+            showPageInfo(meta.currentPage, meta.pageCount, meta.totalCount);
+            
+            nextButton.style.display = meta.isLastPage ? 'none' : 'block';
+            backButton.style.display = meta.isFirstPage ? 'none' : 'block';
+            firstPageButton.style.display = meta.isFirstPage ? 'none' : 'block';
+            lastPageButton.style.display = meta.isLastPage ? 'none' : 'block';
+        } 
+    } catch (error) {
+        console.error('Error loading posts: ', error);
+    }
+}
+
+
+
+function showPageInfo(currentPage, totalPages, totalCount) {
+    const pageInfoElement = document.getElementById('pageInfo');
+    if (pageInfoElement) {
+        pageInfoElement.textContent = `Page ${currentPage} of ${totalPages} - Total Posts: ${totalCount}`;
+    } else {
+        console.error('No page info element found.');
+    }
+}
+
+
+
+
+async function init() {
     const {posts} = await readPosts();
     renderSocialPosts(posts);
     // console.log(posts);
 }
 
-loadPosts();
+loadPosts()
+setLogoutListener();
+authGuard();
+init();
