e929d271317526d1bbd2b90a5bb716c926883ac5
annechh <142426482+annechh@users.noreply.github.com>
2024-09-20 18:57:07 +0200

    Updated the html for build single post

--- src/js/ui/dom/singlePost.js
+++ src/js/ui/dom/singlePost.js
@@ -4,16 +4,52 @@ import {
     createHeadingElement, 
     createElementParagraph } from "./domUtils";
 
-export function buildSinglePost() {
+    // import { readSinglePost } from "../../api/post/read";
+
+// createDivElement({className = '', id = ''})
+
+// createImageElement({className = '', id = '', src = '', alt = ''})
+
+// createHeadingElement({className = '', htmlElement, textContent})
+
+// createElementParagraph({className = '', textContent})
+
+export function buildSinglePost(postData) {
     const renderPost = document.getElementById('post');
 
-    const singlePost = createDivElement({
-        className: 'single-post'
+    const imageContainer = createDivElement({
+        className: 'single-image-container'
     });
 
-    
-    renderPost.append(singlePost)
+    const textContainer = createDivElement({
+        className: 'single-text-container'
+    });
 
-    console.log(renderPost);
+    const postImage = createImageElement({
+        src: postData.media.url, 
+        alt: postData.media.alt
+    }); 
     
+    const postTitle = createHeadingElement({
+        className: 'singlePost-heading',
+        HTMLElement: 'h2', 
+        textContent: postData.title
+    });
+
+    const postText = createElementParagraph({
+        className: 'singlePost-text', 
+        textContent: postData.body
+    });
+
+    const postTags = createElementParagraph({
+        textContent: postData.tags
+    });
+
+
+    textContainer.append(postTitle, postText, postTags)
+    imageContainer.appendChild(postImage)
+    renderPost.append(imageContainer, textContainer)
+
+    return renderPost;
 }
+
