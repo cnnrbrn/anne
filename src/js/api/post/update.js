210810b75c0f4315ede8b769e9196fd532a3ed97
annechh <142426482+annechh@users.noreply.github.com>
2024-09-25 14:35:51 +0200

    feat: add updatePost function to handle updating posts via API

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,28 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+export async function updatePost(id, { title, body, tags, media }) {
+    id = new URLSearchParams(window.location.search).get('id');
+
+    try {
+        const response = await fetch (`${API_SOCIAL_POSTS}/${id}`, {
+            method: 'PUT',
+            headers: headers(),
+            body: JSON.stringify({title, body, tags, media})
+        });
+
+        if (response.ok) {
+            alert('Post successfully updated')
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to update the post:', error);
+    }
+}
+
+
+
+
+console.log('api/post/update.js');
