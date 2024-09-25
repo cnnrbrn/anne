f38aa1350308a7d64b1167dba6b9c84eb7bfb24f
annechh <142426482+annechh@users.noreply.github.com>
2024-09-25 16:12:35 +0200

    feat: add eventListener to edit button using id and redirect to edit page

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -4,10 +4,18 @@ import { buildSinglePost } from "../../ui/dom/singlePost";
 import { readSinglePost } from "../../api/post/read";
 import { setLogoutListener } from "../../ui/global/logout";
 
+const id = new URLSearchParams(window.location.search).get('id');
+
+const editButton = document.getElementById('editPostButton');
+    editButton.addEventListener('click', () => {
+        window.location.href = `/post/edit/?id=${id}`;
+    });
+
+
 async function loadPost() {
     const post = await readSinglePost();
     buildSinglePost(post)
-    console.log('load post',post);
+    console.log('load post in post.js',post);
 }
 
 loadPost();
