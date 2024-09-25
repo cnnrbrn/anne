cdb68d2526aa895243d20538660577c23471f20e
annechh <142426482+annechh@users.noreply.github.com>
2024-09-25 14:35:11 +0200

    feat: add onUpdatePost function to handle post editing and form submission

--- src/js/ui/post/update.js
+++ src/js/ui/post/update.js
@@ -1,1 +1,25 @@
-export async function onUpdatePost(event) {}
+import { updatePost } from "../../api/post/update";
+
+export async function onUpdatePost(event) {
+    event.preventDefault();
+    const id = new URLSearchParams(window.location.search).get('id');
+
+    const form = new FormData(event.target);
+
+    const editedData = {
+        title: form.get('postTitle'),  
+        body: form.get('postText'),    
+        tags: form.get('tags') ? form.get('tags').split(', ').map(tag => tag.trim()) : [],  
+        media: {
+            url: form.get('image'),  
+            alt: form.get('alt') 
+        }
+    };
+    
+    console.log("Formed new Data:", editedData); 
+    updatePost(id, editedData)
+}
+
+
+
+console.log('ui/post/update.js');
