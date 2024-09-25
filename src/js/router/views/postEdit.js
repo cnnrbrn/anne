34ee0f6a88ded0035eb4ebb1e91d68d2877450d9
annechh <142426482+annechh@users.noreply.github.com>
2024-09-25 14:40:39 +0200

    feat: add viewFormData for populating post data and handle form submission for post update

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,3 +1,27 @@
 import { authGuard } from "../../utilities/authGuard";
+import { setLogoutListener } from "../../ui/global/logout";
+import { onUpdatePost } from "../../ui/post/update";
+import { readSinglePost } from "../../api/post/read";
 
+
+async function viewFormData() {
+    const post = await readSinglePost();
+
+    if (post) {
+        document.getElementById('postTitle').value = post.title || '';
+        document.getElementById('postText').value = post.body || '';
+        document.getElementById('tags').value = post.tags ? post.tags.join(', ') : ''; 
+        document.getElementById('imageUrl').value = post.media?.url || ''; 
+        document.getElementById('imageAlt').value = post.media?.alt || '';
+    };
+};
+
+setLogoutListener();
 authGuard();
+viewFormData();
+
+
+const form = document.forms.editPost;
+form.addEventListener("submit", onUpdatePost);
+
+console.log('router/views/postEdit.js');
