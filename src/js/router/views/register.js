0be04b4b3d05f4aef1259e6d7b83a3217c84fa7e
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:23:31 +0200

    chore: add import for buildNavBar function

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -1,5 +1,9 @@
 import { onRegister } from "../../ui/auth/register";
+import { buildNavBar } from "../../ui/dom/nav";
 
 const form = document.forms.register;
 
 form.addEventListener("submit", onRegister);
+
+
+buildNavBar();
