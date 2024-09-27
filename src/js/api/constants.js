b2a3afe876bc3a19de6a0cf2267a1dba66704549
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 20:52:20 +0200

    refactor: reorganize constants and add display state helpers

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -2,10 +2,6 @@
 // In Workflow we will learn how to secure this information
 export const API_KEY = "7166711d-5e4e-46d3-8962-1865ce75a3e2";
 
-export const accessToken = localStorage.getItem('accessToken');
-
-export const userData = JSON.parse(localStorage.getItem('userData'));
-
 export const API_BASE = "https://v2.api.noroff.dev";
 
 export const API_AUTH = `${API_BASE}/auth`;
@@ -23,3 +19,13 @@ export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
 export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
 
 
+
+export const accessToken = localStorage.getItem('accessToken');
+
+export const userData = JSON.parse(localStorage.getItem('userData'));
+
+
+
+export const DISPLAY_BLOCK = localStorage.accessToken ? 'block' : 'none';
+
+export const DISPLAY_NONE = localStorage.accessToken ? 'none' : 'block';
