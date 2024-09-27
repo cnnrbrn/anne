02639b96b1121934a65f3035e6b6f6b5c51c9be4
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 18:40:00 +0200

    chore: remove unused event listener for edit button

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,17 +1,7 @@
-// alert("Single Post Page");
-
 import { buildSinglePost } from "../../ui/dom/singlePost";
 import { readSinglePost } from "../../api/post/read";
 import { setLogoutListener } from "../../ui/global/logout";
 import { buildNavBar } from "../../ui/dom/nav";
-
-
-const id = new URLSearchParams(window.location.search).get('id');
-
-const editButton = document.getElementById('editPostButton');
-    editButton.addEventListener('click', () => {
-        window.location.href = `/post/edit/?id=${id}`;
-    });
 
 
 async function loadPost() {
