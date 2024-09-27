29bdee8297629e083aac78e19dec637ee03ff1c6
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:09:24 +0200

    fix: improve error handling for failed register attempts

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,27 +2,29 @@ import { API_AUTH_REGISTER } from "../constants";
 import { headers } from "../headers";
 
 export async function register({ name, email, password}) {
-  const body = JSON.stringify({
-    name, 
-    email, 
-    password
-  });
-
-  try {
-    const response = await fetch(API_AUTH_REGISTER, {
-      method: 'POST',
-      headers: headers(), 
-      body,
+    const body = JSON.stringify({
+        name, 
+        email, 
+        password
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      window.location.href = '/auth/login/'
-      return data;
-    } 
-  } catch (error) {
-    alert('Could not register new user account')
-    console.log(error);
-  }
-}
+    try {
+        const response = await fetch(API_AUTH_REGISTER, {
+            method: 'POST',
+            headers: headers(), 
+            body,
+        });
 
+        if (!response.ok) {
+            alert('Failed to register user, please try again')
+        } else {
+            alert(`User ${name}  registered successfully`)
+            const data = await response.json();
+            window.location.href = '/auth/login/'
+            return data;
+        }
+    } catch (error) {
+        alert('Could not register new user account')
+        console.log(error);
+    }
+}
