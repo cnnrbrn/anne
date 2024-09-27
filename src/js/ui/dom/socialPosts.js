24bc47257745f6a9227fa24010f564ec7fd45801
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 20:49:34 +0200

    feat: add comments count to social posts cards

--- src/js/ui/dom/socialPosts.js
+++ src/js/ui/dom/socialPosts.js
@@ -4,69 +4,67 @@ import {
     createHeadingElement, 
     createElementParagraph } from "./domElements";
 
-// createDivElement({className = '', id = ''})
-
-// createImageElement({className = '', id = '', src = '', alt = ''})
-
-// createHeadingElement({className = '', htmlElement, textContent})
-
-// createElementParagraph({className = '', textContent})
 
 export function buildSocialPostsCards(postData) {
 
-        const postCard = createDivElement({
-            className: 'post-card'
-        });
-
-        const contentContainer = createDivElement({  });
-
-        contentContainer.addEventListener('click', () => {
-            window.location.href = `/post/?id=${postData.id}`
-        })
-
-        const imageContainer = createDivElement({
-            className: 'card-image-container'
-        });
-
-        if (postData.media && postData.media.url) {
-            const postImage = createImageElement({
-                className: 'post-card-image',
-                id: postData.id,
-                src: postData.media.url,
-                alt: postData.media.alt
-            });
-            
-            imageContainer.appendChild(postImage);
-        } 
-
-        const textContainer = createDivElement({
-            className: 'card-text-container',
-            id: 'cardTextContainer'
-        });
-
-        const postTitle = createHeadingElement({
-            className: 'post-card-title',
-            htmlElement: 'h2',
-            textContent: postData.title
-        });
-
-        const postText = createElementParagraph({
-            className: 'post-card-text',
-            textContent: postData.body
-        });
-
-        const postAuthor = createElementParagraph({
-            textContent: 'Post by: ' + postData.author.name
-        });
-
-        const buttonContainer = createDivElement({
-            className: 'singlePostButtons'
+    const postCard = createDivElement({
+        className: 'post-card'
+    });
+    
+    const contentContainer = createDivElement({  });
+    
+    contentContainer.addEventListener('click', () => {
+        window.location.href = `/post/?id=${postData.id}`
+    })
+    
+    const imageContainer = createDivElement({
+        className: 'card-image-container'
+    });
+    
+    if (postData.media && postData.media.url) {
+        const postImage = createImageElement({
+            className: 'post-card-image',
+            id: postData.id,
+            src: postData.media.url,
+            alt: postData.media.alt
         });
         
-        
-        textContainer.append(postTitle, postText, postAuthor)
-        contentContainer.append(imageContainer, textContainer)
-        postCard.append(contentContainer, buttonContainer);
+        imageContainer.appendChild(postImage);
+    } 
+    
+    const textContainer = createDivElement({
+        className: 'card-text-container',
+        id: 'cardTextContainer'
+    });
+    
+    const postTitle = createHeadingElement({
+        className: 'post-card-title',
+        htmlElement: 'h2',
+        textContent: postData.title
+    });
+    
+    const postText = createElementParagraph({
+        className: 'post-card-text',
+        textContent: postData.body
+    });
+    
+    const postAuthor = createElementParagraph({
+        textContent: 'Post by: ' + postData.author.name
+    });
+    
+    const buttonContainer = createDivElement({
+        className: 'singlePostButtons'
+    });
+    
+    const commentsContainer = createElementParagraph({
+        id: 'commentsContainer',
+        textContent: 'Comments: ' +  postData._count.comments
+    });
+    
+    
+    textContainer.append(postTitle, postText, postAuthor, commentsContainer);
+    contentContainer.append(imageContainer, textContainer)
+    postCard.append(contentContainer, buttonContainer);
     
     return postCard;
 }
