62921631e4369b275db39a8144513451341a1d25
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:20:57 +0200

    chore: add import for buildNavBar and setLogoutListener functions

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,8 @@
+import { buildNavBar } from "../../ui/dom/nav";
+import { setLogoutListener } from "../../ui/global/logout";
 import { authGuard } from "../../utilities/authGuard";
 
+
+buildNavBar();
+setLogoutListener();
 authGuard();
