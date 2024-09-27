20f836498060057727b13c59772bbc608d185c38
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:11:52 +0200

    feat: add deletePost function to handle post deletion

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,29 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+
+
+
+export async function deletePost() {
+    const id = new URLSearchParams(window.location.search).get('id');
+    try {
+        const response = await fetch(`${API_SOCIAL_POSTS}/${id}`, {
+            method: 'DELETE',
+            headers: headers()
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            console.error('Failed to delete post:', errorMessage);
+            return false;
+        } else {
+            alert('Post successfully deleted! Redirecting you to home page');
+            window.location.href = '/'
+        }
+    } catch (error) {
+        console.error('Error deleting post:', error);
+        return false;
+    }
+}
+
+
