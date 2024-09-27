10b38414312c21281049b96d0efc4196edd77e63
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:20:03 +0200

    chore: add import for buildNavBar function

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,5 +1,9 @@
 import { onLogin } from "../../ui/auth/login";
+import { buildNavBar } from "../../ui/dom/nav";
 
 const form = document.forms.login;
 
 form.addEventListener("submit", onLogin);
+
+
+buildNavBar();
