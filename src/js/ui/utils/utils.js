2adb6212113b0ffa59a095f0813fe891f6db5d22
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:40:47 +0200

    chore: remove file due to relocation

--- src/js/ui/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatTags(tags) {
-    if (!tags || tags.length === 0) return '';
-
-    return tags
-        .map(tag => tag.trim().toLowerCase())   
-        .map(tag => tag.charAt(0).toUpperCase() + tag.slice(1)) 
-        .join(', ');  
-}
