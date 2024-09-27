deaf8c3ae670cdc16a28ec4359e7028e50a07eb0
annechh <142426482+annechh@users.noreply.github.com>
2024-09-27 12:29:15 +0200

    feat: add confirmation prompt before deleting a post

--- src/js/ui/post/delete.js
+++ src/js/ui/post/delete.js
@@ -1,1 +1,15 @@
-export async function onDeletePost(event) {}
+import { deletePost } from "../../api/post/delete";
+
+
+export async function onDeletePost(id) {
+    const confirmDelete = confirm('Are you sure you want to delete this post?');
+    
+    if (confirmDelete) {
+        await deletePost(id); 
+    } else {
+        console.log('User canceled the delete action.');
+    }
+}
+
+
+
