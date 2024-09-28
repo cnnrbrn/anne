cd8bc7ca292e6632f72307c2c8343175b53943b8
annechh <142426482+annechh@users.noreply.github.com>
2024-09-29 01:42:20 +0200

    chore: update authGuard function with return true

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,6 +1,7 @@
 export function authGuard() {
   if (!localStorage.accessToken) {
-    alert("You must be logged in to view this page");
-    window.location.href = "/auth/";
+    // alert('You must be logged in to view this page');
+    window.location.href = '/auth/';
   }
+  return true;
 }
