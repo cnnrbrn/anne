fad6560efc750b892bc1b6ea6f23298912eb1483
annechh <142426482+annechh@users.noreply.github.com>
2024-09-29 01:43:21 +0200

    refactor: optimize homepage loading with async function and auth check

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,12 +1,15 @@
-import { setLogoutListener } from "../../ui/global/logout";
-import { authGuard } from "../../utilities/authGuard";
-import { buildNavBar } from "../../ui/dom/nav";
-import { loadPosts } from "../../utilities/pagination";
+import { setLogoutListener } from '../../ui/global/logout';
+import { authGuard } from '../../utilities/authGuard';
+import { buildNavBar } from '../../ui/dom/nav';
+import { loadPosts } from '../../utilities/pagination';
 
+async function loadHomePage() {
+  if (!authGuard()) {
+    return;
+  }
+  await loadPosts();
+  buildNavBar();
+  setLogoutListener();
+}
 
-
-
-loadPosts();
-buildNavBar();
-setLogoutListener();
-authGuard();
+loadHomePage();
