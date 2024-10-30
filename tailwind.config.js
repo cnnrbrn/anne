e9b8c24c07f5f1175b75c93d9ee3f8d6859f47d5
annechh <142426482+annechh@users.noreply.github.com>
2024-10-30 18:27:51 +0100

    Adds dropShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         purpleLight: '#311C36',
         white: '#d9d9d9',
       },
+      dropShadow: {
+        white: '0 5px 10px rgba(217, 217, 217, 15)',
+      },
     },
   },
   plugins: [],
