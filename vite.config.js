84d9d65d52405332ccab53e9c57ccd3548d1fd6a
annechh <142426482+annechh@users.noreply.github.com>
2024-11-06 12:55:42 +0100

    Test: checks if the base path is correct now

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   appType: 'mpa',
-  base: '/fed2-js2-ca-annechh/',
+  base: '/',
   build: {
     target: 'esnext',
     rollupOptions: {
