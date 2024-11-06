7d25364a1351848864a698b2a581f2ab76bd0233
Connor O'Brien <connorbobrien@gmail.com>
2024-11-06 12:20:34 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  appType: 'mpa',
-  base: process.env.NODE_ENV === 'production' ? '/fed2-js2-ca/annechh' : '/',
+  appType: "mpa",
+  // base: process.env.NODE_ENV === 'production' ? '/fed2-js2-ca/annechh' : '/',
+  // base: "anne",
   build: {
-    target: 'esnext',
+    target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './index.html'),
-        login: resolve(__dirname, './auth/login/index.html'),
-        auth: resolve(__dirname, './auth/index.html'),
-        register: resolve(__dirname, './auth/register/index.html'),
-        profile: resolve(__dirname, './profile/index.html'),
-        post: resolve(__dirname, './post/index.html'),
-        editPost: resolve(__dirname, './post/edit/index.html'),
-        createPost: resolve(__dirname, './post/create/index.html'),
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        auth: resolve(__dirname, "./auth/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        post: resolve(__dirname, "./post/index.html"),
+        editPost: resolve(__dirname, "./post/edit/index.html"),
+        createPost: resolve(__dirname, "./post/create/index.html"),
       },
     },
   },
